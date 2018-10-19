7fed7384f71f960a9b149f6430e5b3d24a14e71d
nicoolasaraujo <nicolas.araujo@ufu.br>
2018-10-19 09:03:31 -0300

    iniciando exec4

---
+++ TreinamentoAula1/Models/MD_Enumerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreinamentoAula1.Models
+{
+    public enum ActiveFiler : int
+    {
+        ATIVOS = 0,
+        INATIVOS = 1,
+        TODOS = -1
+    }
+}
