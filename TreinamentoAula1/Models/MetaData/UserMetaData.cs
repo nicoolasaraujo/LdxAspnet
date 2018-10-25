1e44d111771bc3300717acc952d17545a8cc3930
nicoolasaraujo <nicolas.araujo@ufu.br>
2018-10-25 00:23:28 -0300

    Implementação da validação para cadastro/Edição de usuário e implemenação na apresentação da tabela com os mesmos

---
+++ TreinamentoAula1/Models/MetaData/UserMetaData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TreinamentoAula1.Models.MetaData
+{
+    public class UserMetaData
+    {
+        public int id { get; set; }
+
+        [Display(Name = "name", ResourceType = typeof(Resources.Strings))]
+        [Required(ErrorMessageResourceType = typeof(Resources.Strings), ErrorMessageResourceName = "required")]
+        public string name { get; set; }
+
+        [Display(Name = "email", ResourceType = typeof(Resources.Strings))]
+        [Required(ErrorMessageResourceType = typeof(Resources.Strings), ErrorMessageResourceName = "required")]
+        [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$",
+                ErrorMessageResourceType = typeof(Resources.Strings),
+                ErrorMessageResourceName = "invalidEmail")]
+        public string email { get; set; }
+
+        [Display(Name = "ldap_uid", ResourceType = typeof(Resources.Strings))]
+        [Required(ErrorMessageResourceType = typeof(Resources.Strings), ErrorMessageResourceName = "required")]
+        public string ldap_uid { get; set; }    
+
+        public bool ative { get; set; }
+
+        [Display(Name = "password", ResourceType = typeof(Resources.Strings))]
+        [Required(ErrorMessageResourceType = typeof(Resources.Strings), ErrorMessageResourceName = "required")]
+        public string password { get; set; }
+    }
+}
